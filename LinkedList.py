d1eea0bca0a485802e9c69a3b08087a15a1133f9
kamalesh_k_b <59091401+kamalesh-kb@users.noreply.github.com>
2022-08-27 16:37:22 +0530

    linked list basic template created

--- LinkedList.py
+++ LinkedList.py
@@ -1,1 +1,25 @@
-print('Hello World')
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=' -> ')
+            temp = temp.next
+
+l = LinkedList()
+l.head = Node(1)
+second = Node(2)
+third = Node(3)
+
+l.head.next = second
+second.next = third
+
+l.display()
